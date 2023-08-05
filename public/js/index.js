84e1db5f3824b944aeb8acbe87d878dd37683d75
allan-12 <hei.allan.3@gmail.com>
2023-08-05 10:47:05 +0300

    feat: add client interface

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+
+const observe = new IntersectionObserver((entries) => {
+
+entries.forEach((entry)=> {
+        if (entry.isIntersecting){
+            entry.target.classList.add("show")
+        }else{
+            entry.target.classList.remove("show")
+        };
+});
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observe.observe(el));
+
